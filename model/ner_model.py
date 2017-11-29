fdc3e483ace18af55ba09a24ca4344c898f7a961
Arpita <arpita@Arpitas-MBP.wv.cc.cmu.edu>
2017-11-28 21:19:38 -0500

    added evaluation per token

--- model/ner_model.py
+++ model/ner_model.py
@@ -311,7 +311,8 @@ class NERModel(BaseModel):
 
         """
         accs = []
-        correct_preds, total_correct, total_preds = 0., 0., 0.
+        chunk_correct_preds, chunk_total_correct, chunk_total_preds = 0., 0., 0.
+        correct_preds, total_preds, total_correct = 0., 0., 0.
         for words, labels in minibatches(test, self.config.batch_size):
             labels_pred, sequence_lengths = self.predict_batch(words)
 
@@ -325,16 +326,32 @@ class NERModel(BaseModel):
                 lab_pred_chunks = set(get_chunks(lab_pred,
                                                  self.config.vocab_tags))
 
-                correct_preds += len(lab_chunks & lab_pred_chunks)
-                total_preds   += len(lab_pred_chunks)
-                total_correct += len(lab_chunks)
+
+                chunk_correct_preds += len(lab_chunks & lab_pred_chunks)
+                chunk_total_preds   += len(lab_pred_chunks)
+                chunk_total_correct += len(lab_chunks)
+
+
+                #keyphrase evaluation
+                for (a, b) in zip(lab, lab_pred):
+                    if a != "O":
+                        total_correct += 1
+                        if "KP" in a and "KP" in b:
+                            correct_preds += 1
+
+                    if b!= "O":
+                        total_preds += 1
+
+        chunk_p = chunk_correct_preds / chunk_total_preds if chunk_correct_preds > 0 else 0
+        chunk_r = chunk_correct_preds / chunk_total_correct if chunk_correct_preds > 0 else 0
+        chunk_f1 = 2 * chunk_p * chunk_r / (chunk_p + chunk_r) if chunk_correct_preds > 0 else 0
 
         p   = correct_preds / total_preds if correct_preds > 0 else 0
         r   = correct_preds / total_correct if correct_preds > 0 else 0
         f1  = 2 * p * r / (p + r) if correct_preds > 0 else 0
         acc = np.mean(accs)
 
-        return {"acc": 100*acc, "f1": 100*f1}
+        return {"acc": 100*acc, "chunk_f1": 100*chunk_f1, "f1": 100*f1, "p": 100*p, "r": 100*r}
 
 
     def predict(self, words_raw):
